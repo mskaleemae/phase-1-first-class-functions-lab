e3e80f2bfb4ae2758d939f005eddaedd56f42926
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-08-31 12:21:24 -0400

    commit tree 45ae0a3678a9d92a60535bd5974466feba3161d2

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+// Code your solution in this file
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+//1
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+};
+
+//2
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+};
+
+//3
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//4
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+      return multiplyValue * value;
+    };
+  };
+  
+  //5
+  const fareDoubler = createFareMultiplier(2);
+  
+  //6
+  const fareTripler = createFareMultiplier(3);
+  
+  //7
+  const selectDifferentDrivers = function (drivers, driversToReturn) {
+    return driversToReturn(drivers);
+  };
